56115f83b16b6ce1b52085f5d2fd8435a0f96f75
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2024-01-06 01:00:51 +0100

    add test connection

--- src/main/java/servlet/ConnexionServlet.java
+++ src/main/java/servlet/ConnexionServlet.java
@@ -29,7 +29,7 @@ public class ConnexionServlet extends HttpServlet
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         String mail = request.getParameter("mail");
         String motDePasse = request.getParameter("motDePasse");
