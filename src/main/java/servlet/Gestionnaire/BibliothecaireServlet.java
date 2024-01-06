e1a19b6e25b045981080d497a5671fdb1d179633
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2024-01-06 01:24:52 +0100

    add test biblioteque

--- src/main/java/servlet/Gestionnaire/BibliothecaireServlet.java
+++ src/main/java/servlet/Gestionnaire/BibliothecaireServlet.java
@@ -37,7 +37,7 @@ public class BibliothecaireServlet extends HttpServlet
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         String mail = request.getParameter("mail");
 
